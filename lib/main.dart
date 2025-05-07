9da99b5b567011ed831665f5d73048bda06e5bc5
Gabriel Silvestre <gabrielsilvestre1164@gmail.com>
2025-05-07 20:15:06 -0300

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Reserva de Consultórios',
+      title: 'MoveFy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_return_type_for_catch_error
+
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -12,27 +14,78 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-/*  ESTE TRECHO DEVE SER PASSADO PARA A PASTA DE **SERVICES**
- Future<void> _login() async {
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-      
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Erro ao fazer login. Verifique suas credenciais.")),
-      );
-    }
-  }
-  */
+//------------------------------------------------------------------------------
+//ESTA FUNÇÃO DEVE SER PORTADO PARA O CODIGO NA PASTA SERVICES FUTURAMENTE
+  Future<void> _login() async {
+if (_emailController.text.trim().isNotEmpty && 
+    _passwordController.text.trim().isNotEmpty) {
+  
+  final userCredential = await _auth.signInWithEmailAndPassword(
+    email: _emailController.text.trim(),
+    password: _passwordController.text.trim(),
+  ).catchError((error) => null); 
 
+  // ignore: unnecessary_null_comparison
+  if (userCredential != null) {
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  } else {
+
+      Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+}
+//-----------------------------------------------------
+}
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Login Screen")));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _login,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 80),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
